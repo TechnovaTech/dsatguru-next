41b30503d8aa6aae6047e4ba9241266d6db0fbff
Yash Kathiriya <yashkathiriya370@gmail.com>
2026-03-11 03:49:33 +0530

    fix

--- app/api/admin/tutor/tests/update/route.js
+++ app/api/admin/tutor/tests/update/route.js
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server'
-import { connectDB } from '../../../../../lib/db'
-import Test from '../../../../../lib/models/Test'
-import { getTokenFromRequest, verifyToken } from '../../../../../lib/auth'
+import { connectDB } from '../../../../../../lib/db'
+import Test from '../../../../../../lib/models/Test'
+import { getTokenFromRequest, verifyToken } from '../../../../../../lib/auth'
 
 export async function PUT(request) {
   try {
